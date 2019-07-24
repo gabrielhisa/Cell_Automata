55b9eca42f932838af1b93b2182ec70e73b763f7
Gabriel Hisatugu <42539483+gabrielhisa@users.noreply.github.com>
2019-07-24 17:21:43 -0300

    Add colored chars and Von Neumann's neighborhood.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import random
 import time
 import os
 import glob
+from colorama import Fore, init
+init(convert = True)
 
 # 1. Build a board with dead state cells and random state cells
 def dead_state(width,height):
@@ -30,14 +32,22 @@ def random_state(width=150, height=150,threshold=0.5):
         board.append(row)
     return board
 
-# 2. Rendering the board into a pretty printing look
+
+# 2. Rendering the board into a pretty printing look, with the colored option enabled
+def colored_cells():
+    cell_value = '@'
+    colors = list(vars(colorama.Fore).values())
+    colored_cell_value = random.choice(colors) + cell_value
+    return colored_cell_value
+    
 def render(state):
     render_matrix = []
     for row in state:
         render_row = []
         for element in row:
             if element == 1:
-                render_row.append('@')
+                #render_row.append('@')    # Option for monocolored chars
+                render_row.append(colored_cells())
             else:
                 render_row.append(' ')
         render_matrix.append(render_row)
@@ -46,7 +56,8 @@ def render(state):
 
         
 # 3. Applying the rules to a new board, and returning a new state board
-def next_board_state(board):
+def next_board_state_moore(board):
+    # Calculations based on the Moore neighborhood
     board_state = board.copy()
     width = len(board_state[0])
     height = len(board_state)
@@ -98,27 +109,70 @@ def next_board_state(board):
                     new_state[x][y] = 1
     return(new_state)
 
+def next_board_state_neumann(board):
+    # Calculations based in the extended Von Neumann neighborhood
+    board_state = board.copy()
+    width = len(board_state[0])
+    height = len(board_state)
+    new_state = dead_state(width,height)
+    for x in range(0,len(board_state)):
+        for y in range(0,len(board_state[x])):
+            alive_neighbors = 0
+            # Checking upper slot
+            if (x - 1 >= 0):
+                if board_state[x-1][y] == 1:
+                    alive_neighbors += 1
+            # Checking left slot
+            if (y - 1 >= 0):
+                if [y] != [0]:
+                    if board_state[x][y-1] == 1:
+                        alive_neighbors += 1
+            # Checking right slot
+            if (y + 1 < len(board_state[x])):
+                if board_state[x][y+1] == 1:
+                    alive_neighbors += 1
+            # Checking lower slot
+            if (x + 1 < len(board_state)):
+                if board_state[x+1][y] == 1:
+                    alive_neighbors += 1
+            # Deciding what to do with the count of alive neighbors
+            if board_state[x][y] == 1:
+                if alive_neighbors <= 1 or alive_neighbors > 3:
+                    new_state[x][y] = 0
+                else:
+                    new_state[x][y] = 1
+            if board_state[x][y] == 0:
+                if alive_neighbors == 3:
+                    new_state[x][y] = 1
+    return(new_state)
+
 # 4. Run it forever
 def run_forever(initial_state):
     next_state = initial_state
-    while True:
-        render(next_state)
-        next_state = next_board_state(next_state)
-        time.sleep(0.02)
-        os.system('cls')
+    option = int(input('\n\nSelect the neighborhood:\n1 - Moore\'s Neighborhood\n2 - Von Neumann\'s Neighborhood\n'))
+    if option == 1:
+        while True:
+            render(next_state)
+            next_state = next_board_state_moore(next_state)
+            time.sleep(0.001)
+            os.system('cls')
+    if option == 2:
+        while True:
+            render(next_state)
+            next_state = next_board_state_neumann(next_state)
+            time.sleep(0.001)
+            os.system('cls')
 
         
-# 5. This function collects a .txt pattern in the same folder as the script
+# 5. This function collects a .txt pattern in the same directory as the script
 def get_pattern():
     path = os.getcwd()
     extension = 'txt'
     result = glob.glob('*.{}'.format(extension))
     doc_path = path + '\\' + result[0]
-    print(doc_path)
     file = open(doc_path,'r')
     board = []
     for string in file:
-        print(string)
         line = []
         for char in string:
             if char == '0' or char == '1':
@@ -127,6 +181,7 @@ def get_pattern():
         board.append(line)
     return board
 
+# Example for Gosper's Glider Gun
 glider_gun =\
 [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
@@ -163,19 +218,11 @@ glider_gun =\
  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],]
 
-beacon = [[1, 1, 0, 0],
-          [1, 1, 0, 0],
-          [0, 0, 1, 1],
-          [0, 0, 1, 1]]
-
 # 6. Executing
 if __name__ == '__main__':
-    #option = int(input('Select the option:\n1 - Random genereation\n2 - Get pattern from text\n'))
-    option = 1
+    option = int(input('Select the option:\n1 - Random genereation\n2 - Get pattern from text\n3 - Gosper\'s Glider Gun example\n'))
     if option == 1:
         initial_state = random_state()
-        
-    # Width and height not agreeing with the one in the doc, correct    
     if option == 2:
         initial_state = get_pattern()
         height = len(initial_state)
